5bb6803ed0e102bed44a75fb96d7f58dcb8b906f
MakarandPundlik <mmp2308@gmail.com>
2021-05-31 19:54:38 +0530

    part 1 done

---
+++ src/components/Header.jsx
@@ -0,0 +1,61 @@
+import { Box,Avatar,Typography,Grid } from '@material-ui/core';
+import React from 'react';
+import home from '../images/home.png'
+import Typed from 'react-typed';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme=>({
+    avatar:{
+        width:theme.spacing(15),
+        height:theme.spacing(15),
+        margin:theme.spacing(1),
+    },
+    title:{
+        color:"tomato"
+    },
+    subtitle:{
+        color:"tan",
+        marginBottom:"3rem"
+    },
+    typedContainer:{
+        position:"absolute",
+        top:"50%",
+        left:"50%",
+        width:"100vw",
+        transform:"translate(-50%,-50%)",
+        textAlign:"center",
+        zIndex:1
+    }
+}))
+function Header(props) {
+    const classes = useStyles();
+    return (
+        <Box className={classes.typedContainer}>
+            <Grid container justify="center">
+            <Avatar className={classes.avatar} src={home} alt="avatar"/>
+            </Grid>
+            <Typography className={classes.title} variant="h4">
+            <Typed
+                    strings={['Makarand Pundlik']}
+                    typeSpeed={40}
+                />
+                <br/>
+            </Typography>
+            <Typography className={classes.subtitle} variant="h5">
+            <Typed
+                strings={[
+                    'Web Development',
+                    'Full Stack Development',
+                    'Data Structures and Algorithms']}
+                    typeSpeed={30}
+                    backSpeed={40}
+                    
+                    loop >
+                   
+                </Typed>
+            </Typography>
+        </Box>
+    );
+}
+
+export default Header;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import Header from './Header';
-import Navbar from './Navbar';
+import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
+import Particles from "react-particles-js";
+import Header from "./Header";
+import Navbar from "./Navbar";
+
+const useStyles = makeStyles({
+  particlesCanva: {
+    position: "absolute",
+  },
+});
 function Home(props) {
-    return (
-        <div>
-             <Navbar/>
-             <Header/>
-        </div>
-    );
+  const classes = useStyles();
+  return (
+    <>
+      <Navbar />
+
+      <Header />
+      <Particles
+        canvasClassName={classes.particlesCanva}
+        params={{
+          particles: {
+            density: {
+                enable:true,
+                value_ares:900
+            },
+            opacity:{
+                value:1
+            }
+          },
+        }}
+      />
+    </>
+  );
 }
 
 export default Home;
