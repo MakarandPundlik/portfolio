3d0851237edeb18ffd16a3acad6cb1e32ddb097c
MakarandPundlik <mmp2308@gmail.com>
2021-05-31 17:35:39 +0530

    added home header nav sliders

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './Header';
 import Navbar from './Navbar';
 function Home(props) {
     return (
         <div>
              <Navbar/>
+             <Header/>
         </div>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,111 @@
-import React from 'react';
+import React,{useState} from "react";
+import RightMenuSlider from '@material-ui/core/Drawer';
 import {
-    AppBar,
-    Toolbar,
-    ListItem,
-    ListItemText,
-    Box,
-    Typography,
-    List,
-    Divider,
-    IconButton,
-    Avatar
-} from '@material-ui/core';
+  AppBar,
+  Toolbar,
+  ListItem,
+  ListItemText,
+  Box,
+  Typography,
+  List,
+  Divider,
+  IconButton,
+  Avatar,
+  ListItemIcon,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { ArrowBack, Home, ContactMail, Apps } from "@material-ui/icons";
+import home from "../images/home.png";
 
-import { ArrowBack } from '@material-ui/icons';
+const useStyles = makeStyles((theme) => ({
+  menuSliderContainer: {
+    width: 300,
+    background: "#512",
+    height: "100%",
+  },
+  avatar: {
+    width: theme.spacing(13),
+    height: theme.spacing(13),
+    display: "block",
+    margin: "0.5rem auto",
+  },
+  listItem: {
+    color: "tan",
+  },
+}));
+
+const navItems = [
+  {
+    listIcon: <Home />,
+    listText: "Home",
+  },
+  {
+    listIcon: <Apps />,
+    listText: "Technologies",
+  },
+  {
+    listIcon: <ContactMail />,
+    listText: "Contact Me",
+  },
+];
 function Navbar(props) {
-    return (
-        <Box component="nav">
-            <AppBar  position="static" style={{background:"#222"}}>
-                <Toolbar>
-                   <IconButton>
-                   <ArrowBack style={{color:"tan"}}/>
-                   </IconButton>
-                   <Typography variant="h4" style={{color:"tan"}}>
-                       Portfolio
-                   </Typography>
-                </Toolbar>
-            </AppBar>
+  //navbar state
+  const [state,setState] = useState({
+    right:false
+  })
+  const classes = useStyles();
+
+  //side list 
+  const sideList = (slider) => (
+    <Box onClick={toggleNav("right",false)} className={classes.menuSliderContainer} component="div">
+    <Avatar className={classes.avatar} src={home} alt="me" />
+    <Divider />
+    <List>
+      {navItems.map((item, key) => {
+        return (
+          <ListItem button key={key} >
+            <ListItemIcon className={classes.listItem}>
+              {item.listIcon}
+            </ListItemIcon>
+            <ListItemText
+              className={classes.listItem}
+               primary={item.listText}
+            />
+          </ListItem>
+        );
+      })}
+    </List>
+  </Box>
+  )
+
+  //toggler function for navbar
+  const toggleNav = (slider,open)=>()=>{
+      setState({...state,[slider]:open})
+  }
+  return (
+    <>
+     
+      <Box component="nav">
+        <AppBar position="static" style={{ background: "#222" }}>
+          <Toolbar>
+            <IconButton onClick={toggleNav("right",true)}>
+              <ArrowBack style={{ color: "tomato" }} />
+            </IconButton>
+            <Typography variant="h4" style={{ color: "tan" }}>
+              Portfolio
+            </Typography>
+            <RightMenuSlider
+            anchor="right"
+            open={state.right}
+            onClose={toggleNav("right",false)}
+            >
+                {sideList("right")}
+            </RightMenuSlider>
+          </Toolbar>
+        </AppBar>
       </Box>
-    );
+    </>
+  );
 }
 
 export default Navbar;
