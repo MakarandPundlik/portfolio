f4734be7641544c2e62c9e8dad68d3940f1e54ce
MakarandPundlik <mmp2308@gmail.com>
2021-07-03 17:15:24 +0530

    working on techs

--- src/components/IconData.js
+++ src/components/IconData.js
@@ -12,7 +12,7 @@ import cpp from '../images/c++.jpg';
 import github from '../images/github.jpg';
 
 
-export const iconsdata = [
+ const iconsdata = [
     {
         icon:html,
         progress:70,
@@ -23,32 +23,32 @@ export const iconsdata = [
         progress:70,
         title:"css3"
     },
-    {
-        icon:bootstrap,
-        progress:78,
-        title:"Bootstrap5"
-    },
-    {
-        icon:react,
-        progress:85,
-        title:"ReactJs"
-    },
+    // {
+    //     icon:bootstrap,
+    //     progress:78,
+    //     title:"Bootstrap5"
+    // },
+    // {
+    //     icon:react,
+    //     progress:85,
+    //     title:"ReactJs"
+    // },
    
-    {
-        icon:node,
-        progress:80,
-        title:"NodeJs"
-    },
-    {
-        icon:mongo,
-        progress:70,
-        title:"mongoDb"
-    },
-    {
-        icon:mysql,
-        progress:80,
-        title:"MySQL"
-    },
+    // {
+    //     icon:node,
+    //     progress:80,
+    //     title:"NodeJs"
+    // },
+    // {
+    //     icon:mongo,
+    //     progress:70,
+    //     title:"mongoDb"
+    // },
+    // {
+    //     icon:mysql,
+    //     progress:80,
+    //     title:"MySQL"
+    // },
     {
         icon:java,
         progress:90,
@@ -64,213 +64,7 @@ export const iconsdata = [
         icon:c,
         progress:70,
         title:"C"
-    },
-    {
-        icon:cpp,
-        progress:70,
-        title:"C++"
-    },
-    {
-        icon:github,
-        progress:85,
-        title:"GitHub"
-    },
+    }
 ]
+export default iconsdata;
 
-
-export const skills = {
-    data: [
-      {
-        title: "Data Science & AI",
-        fileName: "DataScienceImg",
-        skills: [
-          "⚡ Developing highly scalable production ready models for various deeplearning and statistical use cases",
-          "⚡ Experience of working with Computer Vision and NLP projects",
-          "⚡ Complex quantitative modelling for dynamic forecasting and time series analysis",
-        ],
-        softwareSkills: [
-          {
-            skillName: "Tensorflow",
-            fontAwesomeClassname: "logos-tensorflow",
-            style: {
-              backgroundColor: "transparent",
-            },
-          },
-          {
-            skillName: "Keras",
-            fontAwesomeClassname: "simple-icons:keras",
-            style: {
-              backgroundColor: "white",
-              color: "#D00000",
-            },
-          },
-          {
-            skillName: "PyTorch",
-            fontAwesomeClassname: "logos-pytorch",
-            style: {
-              backgroundColor: "transparent",
-            },
-          },
-          {
-            skillName: "Python",
-            fontAwesomeClassname: "ion-logo-python",
-            style: {
-              backgroundColor: "transparent",
-              color: "#3776AB",
-            },
-          },
-        ],
-      },
-      {
-        title: "Full Stack Development",
-        fileName: "FullStackImg",
-        skills: [
-          "⚡ Building resposive website front end using React-Redux",
-          "⚡ Developing mobile applications using Flutter, React Native and solo android apps using Kotlin",
-          "⚡ Creating application backend in Node, Express & Flask",
-        ],
-        softwareSkills: [
-          {
-            skillName: "HTML5",
-            fontAwesomeClassname: "simple-icons:html5",
-            style: {
-              color: "#E34F26",
-            },
-          },
-          {
-            skillName: "CSS3",
-            fontAwesomeClassname: "fa-css3",
-            style: {
-              color: "#1572B6",
-            },
-          },
-          {
-            skillName: "Sass",
-            fontAwesomeClassname: "simple-icons:sass",
-            style: {
-              color: "#CC6699",
-            },
-          },
-          {
-            skillName: "JavaScript",
-            fontAwesomeClassname: "simple-icons:javascript",
-            style: {
-              backgroundColor: "#000000",
-              color: "#F7DF1E",
-            },
-          },
-          {
-            skillName: "ReactJS",
-            fontAwesomeClassname: "simple-icons:react",
-            style: {
-              color: "#61DAFB",
-            },
-          },
-          {
-            skillName: "NodeJS",
-            fontAwesomeClassname: "simple-icons:node-dot-js",
-            style: {
-              color: "#339933",
-            },
-          },
-          {
-            skillName: "NPM",
-            fontAwesomeClassname: "simple-icons:npm",
-            style: {
-              color: "#CB3837",
-            },
-          },
-          {
-            skillName: "Yarn",
-            fontAwesomeClassname: "simple-icons:yarn",
-            style: {
-              color: "#2C8EBB",
-            },
-          },
-          {
-            skillName: "Gatsby",
-            fontAwesomeClassname: "simple-icons:gatsby",
-            style: {
-              color: "#663399",
-            },
-          },
-          {
-            skillName: "Flutter",
-            fontAwesomeClassname: "simple-icons:flutter",
-            style: {
-              color: "#02569B",
-            },
-          },
-        ],
-      },
-      {
-        title: "Cloud Infra-Architecture",
-        fileName: "CloudInfraImg",
-        skills: [
-          "⚡ Experience working on multiple cloud platforms",
-          "⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases",
-          "⚡ Deploying deep learning models on cloud to use on mobile devices",
-          "⚡ Setting up streaming jobs from DB to Server or vice-versa on GCP and AWS",
-        ],
-        softwareSkills: [
-          {
-            skillName: "GCP",
-            fontAwesomeClassname: "simple-icons:googlecloud",
-            style: {
-              color: "#4285F4",
-            },
-          },
-          {
-            skillName: "AWS",
-            fontAwesomeClassname: "simple-icons:amazonaws",
-            style: {
-              color: "#FF9900",
-            },
-          },
-          {
-            skillName: "Azure",
-            fontAwesomeClassname: "simple-icons:microsoftazure",
-            style: {
-              color: "#0089D6",
-            },
-          },
-          {
-            skillName: "Firebase",
-            fontAwesomeClassname: "simple-icons:firebase",
-            style: {
-              color: "#FFCA28",
-            },
-          },
-          {
-            skillName: "PostgreSQL",
-            fontAwesomeClassname: "simple-icons:postgresql",
-            style: {
-              color: "#336791",
-            },
-          },
-          {
-            skillName: "MongoDB",
-            fontAwesomeClassname: "simple-icons:mongodb",
-            style: {
-              color: "#47A248",
-            },
-          },
-          {
-            skillName: "Docker",
-            fontAwesomeClassname: "simple-icons:docker",
-            style: {
-              color: "#1488C6",
-            },
-          },
-          {
-            skillName: "Kubernetes",
-            fontAwesomeClassname: "simple-icons:kubernetes",
-            style: {
-              color: "#326CE5",
-            },
-          },
-        ],
-      },
-      
-    ],
-  };
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,22 +1,30 @@
-import { Box, Grid, Avatar,Tooltip } from "@material-ui/core";
+import { Box, Grid, Avatar, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
-
-
+import iconsdata from "./IconData";
+import BottomNav from "./BottomNav";
 const useStyles = makeStyles((theme) => ({
-  root:{
-    background:"#233",
-    height:"100vh",
-    width:"100vw"
+  root: {
+    width:"100vw",
+    height:"100vh"
+  },
+  subcontainer:{
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    width: "100vw",
+    transform: "translate(-50%,-50%)",
+    textAlign: "center",
+    
   },
   styledAvatar: {
     height: theme.spacing(7),
     width: theme.spacing(7),
     margin: "1rem",
-    "&:hover":{
-      height:"4rem",
-      width:"4rem"
-    }
+    "&:hover": {
+      height: "4rem",
+      width: "4rem",
+    },
   },
 }));
 
@@ -24,7 +32,27 @@ function Technologies(props) {
   const classes = useStyles();
   return (
     <Box component="div" className={classes.root}>
-     
+      <Box component="div" className={classes.subcontainer}>
+      <Grid container justify="center">
+        <Grid item md={6} lg={6} sm={12} xs={12} xl={6}>
+          <Typography variant="h3" style={{ color: "tan" }}>
+            TECHNOLOGIES I HAVE WORKED WITH
+          </Typography>
+        </Grid>
+        <Grid item md={6} lg={6} sm={12} xs={12} xl={6}>
+        
+          {iconsdata.map((item, key) => {
+            return (
+             
+              <img src={item.icon} key={key} alt="test" height="60" />
+              
+            );
+          })}
+         
+        </Grid>
+      </Grid>
+      </Box>
+     <BottomNav/>
     </Box>
   );
 }
