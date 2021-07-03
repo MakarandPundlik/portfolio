3d53e4bc36f0e43d26fc00de5e17a2f54c64e7cf
MakarandPundlik <mmp2308@gmail.com>
2021-07-03 16:39:06 +0530

    added project stack

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,7 +5,18 @@ import vms from "../images/vms.jpg";
 import java from "../images/java.jpg";
 import covid from "../images/covid.jpg";
 import { makeStyles } from "@material-ui/core/styles";
-import { GitHub, Share } from "@material-ui/icons";
+
+import node from "../images/node.png";
+import javascript from "../images/javascript.png";
+import react from "../images/react.png";
+import mongo from "../images/mongo.png";
+import mysql from "../images/mysql.png";
+
+import html from "../images/html.png";
+import css from "../images/css.png";
+import bootstrap from "../images/bootstrap.png";
+
+import { GitHub } from "@material-ui/icons";
 import {
   Box,
   Typography,
@@ -21,19 +32,15 @@ import {
 const useStyles = makeStyles((theme) => ({
   mainContainer: {
     background: "#233",
-    
-    
   },
   cardContainer: {
     maxWidth: 345,
     margin: "2rem auto",
     background: "#fff",
   },
-  cardTitle: {
-    
-  },
+  cardTitle: {},
   cardContent: {
-   
+    fontSize:"1rem"
   },
 }));
 
@@ -44,6 +51,13 @@ const projects = [
       "A webapp where data of covid-19 is displayed fetched using external APIs. It also displayes data on map and charts for respective countries using chartJs and public APIs of map.",
     github: "https://github.com/MakarandPundlik/covidtracker",
     image: covid,
+    techStack: [
+      "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+      "https://img.shields.io/badge/firebase-ffca28?style=for-the-badge&logo=firebase&logoColor=black",
+      "https://img.shields.io/badge/Chart.js-FF6384?style=for-the-badge&logo=chartdotjs&logoColor=white",
+      "https://img.shields.io/badge/Material--UI-0081CB?style=for-the-badge&logo=material-ui&logoColor=white",
+      "https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white",
+    ],
   },
   {
     title: "BlogPost",
@@ -51,6 +65,14 @@ const projects = [
       "A MERN stack application in ehich user can register himself,create his own blogs and read others blogs,report generation for a registered user.This app providers features such as commenting on blog, total views on blogs.For an administrator it provides admin panel on which admin can see rhe entire report.",
     github: "https://github.com/MakarandPundlik/Blogs",
     image: blogpost,
+    techStack: [
+      "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+      "https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white",
+      "https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white",
+      "https://img.shields.io/badge/Express.js-000000?style=for-the-badge&logo=express&logoColor=white",
+      "https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white",
+      "https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white",
+    ],
   },
   {
     title: "IPR Portal",
@@ -58,6 +80,10 @@ const projects = [
       "A webapp which offers role based views and allows user to register his/her patents.Also, provides chatroom for bidding,displayes the patents accordging to doamin.",
     github: "https://github.com/IPR-Portal",
     image: ipr,
+    techStack: [
+      "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+      "https://img.shields.io/badge/Material--UI-0081CB?style=for-the-badge&logo=material-ui&logoColor=white",
+    ],
   },
   {
     title: "VMS Controls",
@@ -65,6 +91,12 @@ const projects = [
       "A website for a private firm which works into automation industry.A website is build in reactJs with boostrap5 UI components which gives information about company's services, vision & missions, various technologies that compnay has worked with",
     github: "https://github.com/MakarandPundlik/vms",
     image: vms,
+    techStack: [
+      "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+      "https://img.shields.io/badge/Material--UI-0081CB?style=for-the-badge&logo=material-ui&logoColor=white",
+      "https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white",
+      "https://img.shields.io/badge/firebase-ffca28?style=for-the-badge&logo=firebase&logoColor=black",
+    ],
   },
   {
     title: "Core Java",
@@ -72,6 +104,9 @@ const projects = [
       "A github repository which contains all about core java.Covers comcepts like inheritance, abstraction, encapsulation, polymorphism, applets, frames, GUI applications",
     github: "https://github.com/MakarandPundlik/Core-Java",
     image: java,
+    techStack: [
+      "https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=java&logoColor=white",
+    ],
   },
   {
     title: "Advanced Java",
@@ -79,8 +114,11 @@ const projects = [
       "A github repository which contains all about advanced java.Covers comcepts like JDBC, servlets, JSP, multithreading, Collection frameworks, Java beans, java swings, networking in java",
     github: "https://github.com/MakarandPundlik/Advanced-Java",
     image: java,
+    techStack: [
+      "https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=java&logoColor=white",
+      "https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white",
+    ],
   },
-  
 ];
 function Portfolio(props) {
   const classes = useStyles();
@@ -110,8 +148,18 @@ function Portfolio(props) {
                       {item.content}
                     </Typography>
                   </CardContent>
+                  <Grid item xl={4}>
+                    {item.techStack.map((link) => {
+                      return (
+                        <img
+                          src={link}
+                          alt="test"
+                          style={{ margin: "0.2rem" }}
+                        />
+                      );
+                    })}
+                  </Grid>
                   <CardActions>
-                    
                     <Button size="small" href={item.github} target="_blank">
                       <GitHub />
                     </Button>
